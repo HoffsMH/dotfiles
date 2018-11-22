080e90a9eed91da97713fbe968e11a9a66f917c9
HoffsMH <hecker.matthew@gmail.com>
2018-11-21 20:07:32 -0500

    Lots of changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,9 +20,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 (quote
+		("0e8bac1e87493f6954faf5a62e1356ec9365bd5c33398af3e83cfdf662ad955f" default)))
  '(package-selected-packages
-   (quote
-    (evil-collection org evil-mode web-mode use-package syntax-subword smartparens projectile ox-twbs org-bullets oceanic-theme ob-elixir no-littering multiple-cursors move-text htmlize flycheck flx expand-region exec-path-from-shell evil-magit elixir-mode doom-themes diff-hl counsel auto-compile))))
+	 (quote
+		(snazzy-theme evil-collection org evil-mode web-mode use-package syntax-subword smartparens projectile ox-twbs org-bullets oceanic-theme ob-elixir no-littering multiple-cursors move-text htmlize flycheck flx expand-region exec-path-from-shell evil-magit elixir-mode doom-themes diff-hl counsel auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
