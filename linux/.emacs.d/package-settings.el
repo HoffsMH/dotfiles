c93c0bb0096d73ed35cca0acb449986e8382b34f
HoffsMH <matthecker@pm.me>
2019-06-25 18:24:37 -0500

    go

--- linux/.emacs.d/package-settings.el
+++ linux/.emacs.d/package-settings.el
@@ -1,3 +1,4 @@
 (org-babel-load-file (expand-file-name "package-settings.org" "~/personal/dotfiles/common/.emacs.d/"))
 
-(global-set-key (kbd "C-s") 'er/expand-region)
+(global-set-key (kbd "C-e") nil)
+(global-set-key (kbd "C-e") 'er/expand-region)
--- linux/.profile
+++ linux/.profile
@@ -4,3 +4,4 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
 export SESSION=dwm
+
