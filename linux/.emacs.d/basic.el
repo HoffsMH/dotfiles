5572df45ac475eb5223c49a126e923d64cbc956b
HoffsMH <matthecker@pm.me>
2019-06-26 00:41:45 -0500

    solidifying more

--- linux/.emacs.d/basic.el
+++ linux/.emacs.d/basic.el
@@ -1,15 +1,13 @@
 (org-babel-load-file (expand-file-name "basic.org" "~/personal/dotfiles/common/.emacs.d/"))
 
-(eval-after-load 'evil-maps
+(eval-after-load "evil-maps"
 (dolist (map '(evil-motion-state-map
-evil-normal-state-map
-evil-insert-state-map
-evil-emacs-state-map))
-(define-key (eval map) (kbd "C-/") nil)
-(define-key (eval map) (kbd "C-n") nil)
-(define-key (eval map) (kbd "C-z") nil)
-(define-key (eval map) (kbd "C-e") nil))
-(define-key (eval map) "\C-e" nil))
+               evil-insert-state-map
+               evil-emacs-state-map))
+  (define-key (eval map) (kbd "C-e") nil)
+  (define-key (eval map) (kbd "C-/") nil)
+  (define-key (eval map) (kbd "C-n") nil)
+  (define-key (eval map) (kbd "C-z") nil)))
 
 (global-set-key (kbd "C-/") nil)
 (global-set-key (kbd "C-z") nil)
