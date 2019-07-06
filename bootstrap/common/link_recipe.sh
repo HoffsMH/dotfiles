2b999fb2ac904ad57c2c9bf5c586a92d35e46324
HoffsMH <matthecker@pm.me>
2019-07-06 01:00:11 -0500

    better

--- bootstrap/common/link_recipe.sh
+++ bootstrap/common/link_recipe.sh
@@ -10,7 +10,7 @@ ln -sf ~/personal/dotfiles/common/.gitignore_global ~/.
 ln -sf ~/personal/dotfiles/common/.gitconfig ~/.
 ln -sf ~/personal/dotfiles/common/.fzf.zsh ~/.fzf.zsh
 ln -sf ~/personal/dotfiles/common/.zsh ~/.
-ln -sf ~/personal/dotfiles/common/bin ~/
+ln -sf ~/personal/dotfiles/common/bin ~/bin
 
 ln -sf ~/code/util/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/code/util/spacemacs ~/.emacs.d
