d02d0985d227ac51c20b34db408937361317cf9b
HoffsMH <matthecker@pm.me>
2019-07-28 20:16:13 -0400

    lets try that

--- bootstrap/common/link_recipe.sh
+++ bootstrap/common/link_recipe.sh
@@ -18,3 +18,7 @@ ln -sft ~/bin ~/personal/dotfiles/common/bin/*
 ln -sf ~/personal/dotfiles/common/jrnl.makefile ~/personal/jrnl/makefile
 ln -sf ~/code/util/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/code/util/spacemacs ~/.emacs.d
+
+ln -sf ~/pCloudDrive/personal/media/image/library ~/personal/media/image/
+ln -sf ~/pCloudDrive/personal/media/audio/library ~/personal/media/audio
+ln -sf ~/pCloudDrive/personal/media/video/library ~/personal/media/video
