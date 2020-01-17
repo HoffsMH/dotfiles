efc24c202e54ebd781819dc152d5d0f8929e701f
HoffsMH <matthecker@pm.me>
2020-01-17 14:03:44 -0500

    standard language versions

--- bootstrap/linux/misc_admin.sh
+++ bootstrap/linux/misc_admin.sh
@@ -15,7 +15,7 @@ cp -f "$HOME/personal/dotfiles/linux/dlscript.sh" "$HOME/bin/ytgo"
 sudo sed -i '/MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
 sudo sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 
-sudo mkdir -p /usr/local/opt/zlib
+sudo mkdir -p /usr/local/opt/zlib/lib
 
 if [  -f /etc/sudoers.d/10-installer ]; then
   sudo sed -i 's/^\s*\(%wheel\s\+ALL=(ALL)\)\sALL/\1 NOPASSWD: ALL/' /etc/sudoers.d/10-installer
@@ -39,4 +39,14 @@ sudo systemctl start "cpupower"
 
 ./setup_restic.sh
 
-chsh
+
+rbenv install 2.7.0
+exenv install 1.9.4
+nodenv install 13.6.0
+
+cd
+
+rbenv local 2.7.0
+exenv local 1.9.4
+nodenv local 13.6.0
+
