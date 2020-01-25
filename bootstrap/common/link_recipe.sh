bd272b584df06404887a824c400994dca43400b2
HoffsMH <matthecker@pm.me>
2020-01-25 13:11:15 -0500

    go with default dpi on x resources and no more jrnl makefile

--- bootstrap/common/link_recipe.sh
+++ bootstrap/common/link_recipe.sh
@@ -15,7 +15,6 @@ ln -sf ~/personal/dotfiles/common/.fzf.zsh ~/.fzf.zsh
 ln -sf ~/personal/dotfiles/common/.zsh ~/.
 ln -sft ~/bin ~/personal/dotfiles/common/bin/*
 
-ln -sf ~/personal/dotfiles/common/jrnl.makefile ~/personal/jrnl/makefile
 ln -sf ~/code/util/oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/code/util/spacemacs ~/.emacs.d
 
