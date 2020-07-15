c7a7e859a0950ffa0069861da66f6fae131942c1
HoffsMH <matthecker@pm.me>
2020-07-14 23:50:50 -0400

    .

--- bootstrap/misc_admin.sh
+++ bootstrap/misc_admin.sh
@@ -7,6 +7,8 @@ echo "###############################################"
 echo "MISC ADMIN"
 echo "###############################################"
 
+# make thunar our default gui file manager
+xdg-mime  default  thunar.desktop inode/directory
 
 #speed up aur makepkg
 sudo sed -i '/MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
@@ -16,7 +18,6 @@ sudo sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoer
 if [  -f /etc/sudoers.d/10-installer ]; then
   sudo sed -i 's/^\s*\(%wheel\s\+ALL=(ALL)\)\sALL/\1 NOPASSWD: ALL/' /etc/sudoers.d/10-installer
 fi
-
 
 # get the time right
 sudo ntpd -qg
