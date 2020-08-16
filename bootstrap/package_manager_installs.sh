66250f9a0585a9409eba0dd6d7eec8b0e9ab0e0f
HoffsMH <matthecker@pm.me>
2020-08-16 14:40:26 -0500

    .

--- bootstrap/package_manager_installs.sh
+++ bootstrap/package_manager_installs.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-
 #speed up aur makepkg
 sudo sed -i '/MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
 sudo sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
-
 
 echo "###############################################"
 echo "YAY BASIC RECIPE"
