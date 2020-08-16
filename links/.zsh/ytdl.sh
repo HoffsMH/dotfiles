3ace25d7ca0ff7de54b233e80cbb48145fae49bd
HoffsMH <matthecker@pm.me>
2020-08-16 09:31:29 -0500

    youtube dl stuff

--- links/.zsh/ytdl.sh
+++ links/.zsh/ytdl.sh
@@ -44,7 +44,7 @@ ytdlv() {
 
     echo  downloading "$yturl"
 
-    youtube-dl -c --sleep-interval 2 --max-sleep-interval 4 --yes-playlist --no-warnings --playlist-reverse --download-archive ~/.yt-dl-archive/archive -o "~/personal/media/video/capture/%(title)s-%(id)s.%(ext)s" $yturl
+    youtube-dl -c --sleep-interval 2 --max-sleep-interval 4 --yes-playlist --no-warnings --playlist-reverse --download-archive ~/.config/.yt-dl-archive/archive -o "~/personal/media/video/capture/%(title)s-%(id)s.%(ext)s" $yturl
 }
 
 ytdla() {
@@ -53,7 +53,7 @@ ytdla() {
 
     echo  downloading "$yturl"
 
-    youtube-dl  -x  -c --sleep-interval 2 --max-sleep-interval 4 --yes-playlist --no-warnings --playlist-reverse --download-archive ~/.yt-dl-archive/archive --audio-format 'mp3' -i --audio-quality 0 -o "~/personal/media/audio/capture/%(title)s-%(uploader)s-%(id)s.%(ext)s" $yturl
+    youtube-dl  -x  -c --sleep-interval 2 --max-sleep-interval 4 --yes-playlist --no-warnings --playlist-reverse --download-archive ~/.config/.yt-dl-archive/archive --audio-format 'mp3' -i --audio-quality 0 -o "~/personal/media/audio/capture/%(title)s-%(uploader)s-%(id)s.%(ext)s" $yturl
 }
 
 installytdl() {
@@ -68,6 +68,6 @@ ytdlalb() {
 
     echo  downloading "$yturl"
 
-    youtube-dl  -x  -c --sleep-interval 2 --max-sleep-interval 4 --yes-playlist --no-warnings --playlist-reverse --download-archive ~/.yt-dl-archive/archive --audio-format 'mp3' -i --audio-quality 0 -o "~/personal/media/audio/capture/$albumname/%(title)s-%(uploader)s-%(id)s.%(ext)s" $yturl
+    youtube-dl  -x  -c --sleep-interval 2 --max-sleep-interval 4 --yes-playlist --no-warnings --playlist-reverse --download-archive ~/.config/.yt-dl-archive/archive --audio-format 'mp3' -i --audio-quality 0 -o "~/personal/media/audio/capture/$albumname/%(title)s-%(uploader)s-%(id)s.%(ext)s" $yturl
 }
 
