29f062ec8e1dfa9171cec7e4e7ceceb443ca7986
HoffsMH <matthecker@pm.me>
2020-08-22 18:02:06 -0500

    yubikey agent wiring

--- bootstrap/setup_util.sh
+++ bootstrap/setup_util.sh
@@ -14,10 +14,12 @@ rm -rf zsh-autopair
 rm -rf zsh-syntax-highlighting
 rm -rf dwm
 rm -fr asdf
+rm -fr yubikey-agent
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git
 git clone git://github.com/denysdovhan/spaceship-prompt.git
 git clone git://github.com/asdf-vm/asdf.git
+git clone https://aur.archlinux.org/yubikey-agent.git
 
 git clone git://github.com/hlissner/zsh-autopair.git
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
@@ -41,3 +43,12 @@ pushd ~/code/util/asdf
   git checkout "$(git describe --abbrev=0 --tags)"
 popd
 ln -sf "$HOME/code/util/asdf" "$HOME/.asdf"
+
+pushd ~/code/util/yubikey-agent
+  makepkg -si
+  systemctl daemon-reload --user || echo "already reloaded daemon"
+  sudo systemctl enable --now pcscd.socket || echo "already started pcscd"
+  systemctl --user enable --now yubikey-agent | echo "already started yubikey-agent"
+
+  export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/yubikey-agent/yubikey-agent.sock"
+popd
