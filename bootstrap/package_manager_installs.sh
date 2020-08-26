3334f91b90a6ccf97e0858c901e05666c8d467f2
HoffsMH <matthecker@pm.me>
2020-08-26 08:57:31 -0500

    gopass

--- bootstrap/package_manager_installs.sh
+++ bootstrap/package_manager_installs.sh
@@ -13,6 +13,7 @@ echo "###############################################"
 sudo pacman -Syu
 yay -Syu
 yay -S --noconfirm \
+    gopass \
     linux-lts-acpi_call \
     taskell \
     lxappearance \
--- links/.zsh/path_and_shims.sh
+++ links/.zsh/path_and_shims.sh
@@ -14,7 +14,7 @@ export PATH=$ASDF_DATA_DIR/shims:$PATH
 export PATH=/usr/local/go/bin:$PATH
 export GOPATH=$HOME/code/go
 export GOBIN=$HOME/code/go/bin
-
+export PATH=$GOBIN:$PATH
 
 # rust env
 export PATH="$HOME/.cargo/bin:$PATH"
