ba88ba29bbb22369d329e1aea42d03c50f59130d
HoffsMH <matthecker@pm.me>
2020-08-29 22:24:58 -0500

    go

--- bootstrap/links/cleanup.sh
+++ bootstrap/links/cleanup.sh
@@ -1,2 +1,5 @@
 rm -fr "$HOME/.config/sublime-text-3/Packages/gruvbox"
 ln -sf "$HOME/personal/dotfiles/links/.config/sublime-text-3/Packages/gruvbox" "$HOME/.config/sublime-text-3/Packages"
+
+chmod 700 ~/.gnupg/gpg-agent.conf
+chmod 700 ~/.gnupg/gpg.conf
