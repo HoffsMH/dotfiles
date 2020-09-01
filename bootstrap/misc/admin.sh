84ad56030dfb2d647344528d715ebd4cdffa0771
HoffsMH <matthecker@pm.me>
2020-09-01 15:33:58 -0500

    even more better

--- bootstrap/misc/admin.sh
+++ bootstrap/misc/admin.sh
@@ -9,11 +9,6 @@ echo "###############################################"
 # make thunar our default gui file manager
 xdg-mime  default  thunar.desktop inode/directory
 
-#speed up aur makepkg
-sudo sed -i '/MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
-sudo sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
-
-
 if [  -f /etc/sudoers.d/10-installer ]; then
   sudo sed -i 's/^\s*\(%wheel\s\+ALL=(ALL)\)\sALL/\1 NOPASSWD: ALL/' /etc/sudoers.d/10-installer
 fi
@@ -24,3 +19,5 @@ sudo timedatectl set-timezone "$(curl --fail https://ipapi.co/timezone)"
 sudo hwclock -w
 
 sudo usermod -aG docker $USER
+
+"$HOME/personal/dotfiles/bootstrap/misc/remove-cruft.sh"
