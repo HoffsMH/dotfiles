788123fc361f52f4e5a0f9b1b3ac9782d3f19dc8
HoffsMH <matthecker@pm.me>
2021-02-06 23:11:35 -0600

    gtk 3

--- bootstrap/links/cleanup.sh
+++ bootstrap/links/cleanup.sh
@@ -1,5 +1,3 @@
-rm -fr "$HOME/.config/sublime-text-3/Packages/gruvbox"
-ln -sf "$HOME/personal/dotfiles/links/.config/sublime-text-3/Packages/gruvbox" "$HOME/.config/sublime-text-3/Packages"
 
 chmod 700 ~/.gnupg/gpg-agent.conf
 chmod 700 ~/.gnupg/gpg.conf
