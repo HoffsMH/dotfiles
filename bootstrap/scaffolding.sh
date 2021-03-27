93e7a176b5c9fcbc289a3502558eeedf7642e471
HoffsMH <matthecker@pm.me>
2021-03-26 21:07:41 -0500

    go

--- bootstrap/scaffolding.sh
+++ bootstrap/scaffolding.sh
@@ -2,6 +2,11 @@ set -e
 
 sudo sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
 echo "###############################################"
 echo "CREATE DIRS"
 echo "###############################################"
