9be4a4fdd330210fd0e72179160579e5e093a1d9
HoffsMH <matthecker@pm.me>
2021-10-06 09:09:42 -0500

    client focusing

--- links/.config/awesome/rc.lua
+++ links/.config/awesome/rc.lua
@@ -590,7 +590,10 @@ end)
 -- }}}
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:activate { context = "mouse_enter", raise = false }
-end)
+-- used to be: 
+-- client.connect_signal("mouse::enter", function(c)
+
+-- client.connect_signal("mouse::click", function(c)
+--     c:activate { context = "mouse_enter", raise = false }
+-- end)
 
