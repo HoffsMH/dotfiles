d8148721223cb5ca35d84a2923a6e6fed38ab351
HoffsMH <matthecker@pm.me>
2021-10-21 16:03:09 -0500

    annoying

--- links/.zsh/functions_and_aliases.sh
+++ links/.zsh/functions_and_aliases.sh
@@ -76,7 +76,7 @@ gcof() {
             sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
     target=$(
         (echo "$tags"; echo "$branches") |
-            fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2 --preview-window=hidden
+            fzf --no-hscroll --ansi +m -d "\t" -n 2 --preview-window=hidden
           ) || return
     git checkout $(echo "$target" | awk '{print $2}')
 }
