785b91318db724ef742bb6fa9243afe7676aa861
HoffsMH <matthecker@pm.me>
2022-07-02 21:01:46 -0500

    install binlist

--- bootstrap/install/timecat.sh
+++ bootstrap/install/timecat.sh
@@ -14,3 +14,20 @@ git clone https://github.com/HoffsMH/timecat
 pushd ~/code/util/timecat
   ./bin/install
 popd
+
+#! /bin/bash
+
+echo "###############################################"
+echo "Install binlist"
+echo "###############################################"
+
+mkdir -p "$HOME/code/util"
+pushd "$HOME/code/util"
+
+rm -fr timecat
+
+git clone https://github.com/HoffsMH/binlist
+
+pushd ~/code/util/binlist
+  ./bin/install
+popd
