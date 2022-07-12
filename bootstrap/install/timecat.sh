7f399f6915f03130a7bf941824de002d2551732b
HoffsMH <matthecker@pm.me>
2022-07-11 20:14:08 -0500

    timecat now also installs bulksearch

--- bootstrap/install/timecat.sh
+++ bootstrap/install/timecat.sh
@@ -15,7 +15,6 @@ pushd ~/code/util/timecat
   ./bin/install
 popd
 
-#! /bin/bash
 
 echo "###############################################"
 echo "Install binlist"
@@ -24,10 +23,25 @@ echo "###############################################"
 mkdir -p "$HOME/code/util"
 pushd "$HOME/code/util"
 
-rm -fr timecat
+rm -fr binlist
 
 git clone https://github.com/HoffsMH/binlist
 
 pushd ~/code/util/binlist
+  ./bin/install
+popd
+
+echo "###############################################"
+echo "Install bulksearch"
+echo "###############################################"
+
+mkdir -p "$HOME/code/util"
+pushd "$HOME/code/util"
+
+rm -fr bulksearch
+
+git clone https://github.com/HoffsMH/bulksearch
+
+pushd ~/code/util/bulksearch
   ./bin/install
 popd
