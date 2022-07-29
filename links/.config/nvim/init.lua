48be5f60cfccceb94ff95af6748cb477e99ca2d6
HoffsMH <matthecker@pm.me>
2022-07-29 17:19:50 -0500

    fills up fast

--- links/.config/nvim/init.lua
+++ links/.config/nvim/init.lua
@@ -1,5 +1,4 @@
 vim.opt.colorcolumn = "80"
-vim.api.nvim_command([[nnoremap <leader>pv <cmd>Ex<cr>]])
 vim.opt.clipboard = "unnamedplus"
 vim.opt.nu = true
 vim.opt.tabstop = 2
@@ -8,7 +7,13 @@ vim.opt.shiftwidth = 2
 vim.opt.hlsearch = search
 vim.opt.incsearch = true
 vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.wrap = false
+vim.opt.list = true
+vim.opt.foldlevel = 5
+
+vim.opt.listchars="tab:>-,space:.,eol:¬"
 
 vim.g.netrw_banner = 0
 
@@ -16,13 +21,17 @@ local nnoremap = require('keymap').nnoremap
 
 vim.g.mapleader = " "
 
+-- keybind to switch between folding methods so I can prefer syntx
+
 nnoremap("<leader>pv", "<cmd>Ex<cr>")
 nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<cr>")
 nnoremap("<leader>fb", "<cmd>Telescope buffers<cr>")
+nnoremap("<leader>.", "<cmd>e ~/.config/nvim<cr>")
 nnoremap("q:", "<Nop>")
 
-
+-- make this keep expandign
+nnoremap("<C-e>", "<Plug>(expand_region_expand)")
 
 -- to install packer
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim\
@@ -31,17 +40,18 @@ nnoremap("q:", "<Nop>")
 vim.api.nvim_command('packadd packer.nvim')
 
 require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-	use {'dracula/vim', as = 'dracula'}
-	use { 'ellisonleao/gruvbox.nvim', as = 'gruvbox' }
-	use { 'terryma/vim-expand-region', as = 'expand_region' }
-	use 'andymass/vim-matchup'
+  use 'wbthomason/packer.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {'dracula/vim', as = 'dracula'}
+  use { 'ellisonleao/gruvbox.nvim', as = 'gruvbox' }
+  use { 'terryma/vim-expand-region', as = 'expand_region' }
+  use 'andymass/vim-matchup'
   use 'rstacruz/vim-closer'
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-lua/plenary.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'terrortylor/nvim-comment'
+
 end)
 
 require('nvim_comment').setup({
@@ -69,7 +79,8 @@ require('nvim-treesitter.configs').setup {
 	    "c",
 	    "lua",
 	    "javascript",
-	    "go"
+	    "go",
+			"glimmer"
     },
 
 
@@ -85,11 +96,26 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+local vim = vim
+local opt = vim.opt
+
+opt.foldmethod = "indent"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = {"src/parser.c"}
+  },
+  filetype = "gotmpl",
+  used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
+}
+
 
 vim.api.nvim_command([[colorscheme gruvbox]])
 
 
-vim.api.nvim_command([[map <C-e> <Plug>(expand_region_expand)]])
 
 
 
